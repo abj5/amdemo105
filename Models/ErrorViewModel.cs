30170200b4b61528f37a437203365f5df06e7d0e
Adam <adam_b_johnson@yahoo.com>
2021-10-22 13:53:56 -0700

    initial2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace amDemo105.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
